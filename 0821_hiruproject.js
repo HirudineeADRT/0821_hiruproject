0610576d7c26d13b47294164c19b230c180e8890
HirudineeADRT <hirudinee@adroitlogic.com>
2020-10-06 16:15:29 +0530

    test

--- 0821_hiruproject.js
+++ 0821_hiruproject.js
@@ -1,36 +1,24 @@
-const SL_XML = require('slappforge-sdk-xml');
-const xmlParser = new SL_XML.XMLParser();
-const xPathEvaluator = new SL_XML.XPathEvaluator();
 const Alexa = require('ask-sdk');
-const AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient();
+let skill;
 
-exports.handler = async (event) => {
-    console.log("This is a test");
-    let skill;
-
-    exports.handler = async function (event, context) {
-        //console.log('REQUEST ' + JSON.stringify(event));
-        if (!skill) {
-            skill = Alexa.SkillBuilders.custom()
-                .addErrorHandlers(ErrorHandler)
-                .addRequestHandlers(
-                // delete undefined built-in intent handlers
-                CancelIntentHandler,
-                HelpIntentHandler,
-                StopIntentHandler,
-                NavigateHomeIntentHandler,
-                FallbackIntentHandler,
-                LaunchRequestHandler,
-                SessionEndedRequestHandler
-                // add custom Intent handlers
-                ).create();
-        }
-
-        const response = await skill.invoke(event, context);
-        //console.log('RESPONSE :' + JSON.stringify(response));
-        return response;
-    };
+exports.handler = async function (event, context) {
+    //console.log('REQUEST ' + JSON.stringify(event));
+    if (!skill) {
+        skill = Alexa.SkillBuilders.custom()
+            .addErrorHandlers(ErrorHandler)
+            .addRequestHandlers(    
+            // delete undefined built-in intent handlers
+            CancelIntentHandler,
+            HelpIntentHandler,
+            
+            StopIntentHandler,
+            NavigateHomeIntentHandler,
+            FallbackIntentHandler,
+            LaunchRequestHandler,
+            SessionEndedRequestHandler
+            // add custom Intent handlers
+            ).create();
+    }
     const HelpIntentHandler = {
         canHandle(handlerInput) {
             return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -43,52 +31,10 @@ exports.handler = async (event) => {
                 .speak(speechText)
                 .withShouldEndSession(false)    // set to true for Cancel or Stop intents etc
                 .getResponse();
-
-        }
-    };
-    const ErrorHandler = {
-        canHandle(handlerInput) {
-            return true;
-        },
-        handle(handlerInput, error) {
-            console.log('Error handled: ' + JSON.stringify(error.message));
-            // console.log('Original Request was:', JSON.stringify(handlerInput.requestEnvelope.request, null, 2));
-
-            const speechText = 'Sorry, your skill encountered an error';
-            return handlerInput.responseBuilder
-                .speak(speechText)
-                .withShouldEndSession(false)
-                .getResponse();
         }
     };
 
-    try {
-        let data = await ddb.get({
-            TableName: "hirutestddb",
-            Key: {
-                Name: "test"
-            }
-        }).promise();
-
-    } catch (err) {
-        console.log("This is a test");
-
-        // error handling goes here
-    };
-
-    try {
-        let data = await ddb.get({
-            TableName: "hirutestddb",
-            Key: {
-                Name: "test"
-            }
-        }).promise();
-
-    } catch (err) {
-        consoel.log(test);
-        console.test(sample);
-        // error handling goes here
-    };
-
-    return { "message": "Successfully executed" };
+    const response = await skill.invoke(event, context);
+    //console.log('RESPONSE :' + JSON.stringify(response));
+    return response;
 };
--- test/sample.js
+++ test/sample.js
@@ -1,4 +1,18 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    try {
+        let data = await ddb.get({
+            TableName: "hirutestddb",
+            Key: {
+                Name: "test"
+            }
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
 };
